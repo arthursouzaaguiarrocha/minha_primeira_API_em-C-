ec96c3419c9f0cf443a5b5631290e35cc0d9cd25
Arthur Souza Aguiar Rocha <arthurdev.ads@gmail.com>
2026-07-02 16:14:02 -0300

    Add files via upload

---
+++ MinhaPrimeiraAPIemC#/Controllers/MinhaPrimeiraAPIController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MinhaPrimeiraAPIemC_.Controllers
+{
+    [Route("api/minhaprimeiraapi")]
+    [ApiController]
+    public class MinhaPrimeiraAPIController : ControllerBase
+    {
+        [Route("olamundo")]
+        [HttpGet]
+        public string olaMundo()
+        {
+            var nome = "Ola mundo";
+
+            return nome;
+        }
+
+        [HttpGet("olamundopersonalizado")]
+        public string OlaMundoPersonalizado(string nome)
+        {
+            return "Olá " + nome;
+        }
+
+        [HttpGet("somanotas")]
+        public string SomaNotas(int nota1, int nota2)
+        {
+            var resultado = nota1 + nota2;
+
+            return "O resultado é " + resultado;
+        }
+
+        [HttpGet("media")]
+        public string Media(int nota1, int nota2)
+        {
+            var resultado = (nota1 + nota2) / 2;
+            return "O resultado é " + resultado;
+        }
+    }
+}
